be7ca9f5d4dfb68484967b5d6ca83735c12e2b12
hangrybear666 <corey.lauster@gmail.com>
2024-07-10 22:44:49 +0200

    Added OpenJDK Installation Shell Script

---
+++ README.md
@@ -0,0 +1,4 @@
+# Linux Basics on Ubuntu & Mint
+
+This repo contains mostly POSIX compliant shell scripts written with vim in GNOME Terminal
+
---
+++ install-java-17.0.11.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# -y flag autoconfirms instead of waiting for user input
+sudo apt-get -y install openjdk-17-jre-headless
+
+
+which_java=$(which java)
+installations=$(java --version)
+
+if [ -z "$installations" ] || [ -z "$which_java" ]
+  then
+    is_java_installed=false
+  else
+    is_java_installed=true
+fi
+
+if [ "$is_java_installed" = true ]
+  then
+    echo "java install path: $which_java"
+    echo -e "installed java versions: \n$installations"
+  else
+    echo "no java version installed"
+fi
+
+#head -n 1: Takes the first line of input
+#awk -F '"' '{print $2}': Splits Input by double quote character and prints the second field
+java_version_num=$(java -version 2>&1 | grep -i version | head -n 1 | awk -F '"' '{print $2}')
+
+#awk -F '.' '{print $1}': splits the input by dots and prints the first field
+java_major_version=$( echo $java_version_num | awk -F '.' '{print $1}' )
+
+if [ ! -z "$java_major_version" ] && [ "$java_major_version" -ge 11 ]
+  then
+    installation_successful=true
+    echo "Java Installation Successful."
+    echo "java major version: $java_major_version"
+  else
+    installation_successful=false
+    echo "Installation error. Java Major Version 11 or greater not installed."
+fi
