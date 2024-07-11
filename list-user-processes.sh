5fdf68f570ee727a3db828c5777b506bd6465ee8
hangrybear666 <corey.lauster@gmail.com>
2024-07-11 21:23:16 +0200

    Added script to list active user processes

--- list-user-processes.sh
+++ list-user-processes.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-ps aux | grep $(printenv USER | cut -c 1-7 )
+read -p "Enter sort order (3 for cpu, 4 for ram): " order
+
+if [ $order = "3" ]
+then
+  echo "sorting by cpu"
+elif [ $order = "4" ]
+then 
+  echo "sorting by ram"
+else
+  echo "please select either 3 or 4."
+  return
+fi
+
+read -p "Enter number of processes to be displayed: " num_lines
+
+ps aux | head -n 1
+ps aux | grep $(printenv USER | cut -c 1-7 ) | sort -k $order -nr | head -n $num_lines
